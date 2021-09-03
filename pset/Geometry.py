b0f6dd3159172c325bbc2ce6842902796737cb0a
dprua <dprua@naver.com>
2021-09-03 18:51:52 +0900

    pset1,2 버그 수정

--- pset/Geometry.py
+++ pset/Geometry.py
@@ -4,6 +4,10 @@ class Point:
     def __init__(self,x,y):
         self.x = x
         self.y = y
+    def __str__(self):
+        return 'A point at ({}, {})'.format(self.x, self.y)
+    def __repr__(self):
+        return 'Point({}, {})'.format(self.x, self.y)
     def halfway(self,point):
         a = (point.x + self.x) / 2
         b = (point.y + self.y) / 2
@@ -21,6 +25,10 @@ class Point:
 class Square:
     def __init__(self,line):
         self.line = line
+    def __str__(self):
+        return '{} x {} Square'.format(self.line)
+    def __repr(self):
+        return 'Square({})'.format(self.line)
     def perimeter(self):
         return 4*self.line
     def area(self):
@@ -29,6 +37,10 @@ class Square:
 class Circle:
     def __init__(self,r=0):
         self.r = r
+    def __str__(self):
+        return 'A circle with a radius of {}cm'.format(self.r)
+    def __repr(self):
+        return 'Circle({})'.format(self.r)
     def radius(self):
         return self.r
     def diameter(self):
