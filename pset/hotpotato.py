80b2f925016ec20cf85b5a8acc5941fac006e0c3
dprua <dprua@naver.com>
2021-10-13 18:22:04 +0900

    LinkedList 구현

---
+++ pset/hotpotato.py
@@ -0,0 +1,23 @@
+'''
+Honor Code: On my honour, I pledge that I have neither received nor provided improper assistance 
+in the completion of this assignment. 
+Signed: 박예겸 Student Number: 21600277
+'''
+
+from queue import Queue
+
+def hotPotato(namelist, num):
+    que = Queue()
+    for name in namelist: 
+        que.put(name)
+    assert len(namelist) == que.qsize()    
+    while que.qsize() > 1:
+        for i in range(num): 
+            que.put(que.get())
+        que.get()
+    assert que.qsize() == 1
+    return que.get()
+
+if __name__ == "__main__":
+    namelist = [ 'Bill', 'David', 'Susan', 'Jane' ]
+    print("The winner is", hotPotato(namelist, 3))
